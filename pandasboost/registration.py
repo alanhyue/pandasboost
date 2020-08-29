563b23a826a5f2d0a87a2a4495b506f023eaeeba
Heng Yue <y.heng7@gmail.com>
2020-08-29 15:28:26 +0800

    + series booster registration tests

--- pandasboost/registration.py
+++ pandasboost/registration.py
@@ -1,4 +1,4 @@
-from .pandas_flavor import register_dataframe_accessor
+from .pandas_flavor import register_dataframe_accessor, register_series_accessor
 from functools import wraps
 
 
@@ -18,6 +18,27 @@ def register_dataframe_booster(name):
             return f(instance._obj, *args, **kwargs)
 
         setattr(DataFrameBooster, name, wrapper)
+        return f
+
+    return decorator
+
+
+@register_series_accessor("bs")
+class SeriesBooster:
+    def __init__(self, obj):
+        self._obj = obj
+
+
+def register_series_booster(name):
+    def decorator(f):
+        # hack
+        @wraps(f)
+        def wrapper(*args, **kwargs):
+            instance = args[0]
+            args = args[1:]
+            return f(instance._obj, *args, **kwargs)
+
+        setattr(SeriesBooster, name, wrapper)
         return f
 
     return decorator
--- tests/test_registration.py
+++ tests/test_registration.py
@@ -1,5 +1,5 @@
 import pytest
-from pandasboost.registration import register_dataframe_booster
+from pandasboost.registration import register_dataframe_booster, register_series_booster
 
 
 def test_register_dataframe_booster():
@@ -10,4 +10,14 @@ def test_register_dataframe_booster():
         return frame.shape
 
     assert hasattr(pd.DataFrame.bs, "redundant_shape")
+
+
+def test_register_series_booster():
+    import pandas as pd
+
+    @register_series_booster("redundant_count")
+    def count(srs):
+        return srs.size()
+
+    assert hasattr(pd.Series.bs, "redundant_count")
 
